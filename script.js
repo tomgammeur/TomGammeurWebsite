939a7b22f0f7982825bc0e6943d438ed30b4bd8a
Tom Gammeur <101002853+tomgammeur@users.noreply.github.com>
2023-12-05 15:14:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ function toggleLike(btn) {
     likeAnimation.style.display = 'none';
   }, 500);
 }
+function toggleLike(btn) {
+  btn.classList.toggle('liked');
+
+  const likeAnimation = btn.nextElementSibling;
+  likeAnimation.style.display = 'block';
+  setTimeout(() => {
+    likeAnimation.style.display = 'none';
+  }, 500);
+}
+// Contrôle pour cacher la vidéo après la lecture ou une durée spécifique
+const video = document.getElementById('video');
+const videoContainer = document.getElementById('video-container');
+
+video.addEventListener('ended', () => {
+  // Cache la vidéo lorsque la lecture est terminée
+  videoContainer.style.display = 'none';
+  // Ajoutez ici le code pour rediriger l'utilisateur vers la page principale du site, par exemple :
+  // window.location.href = 'page_principale.html';
+});
+
+// Ou pour cacher la vidéo après un certain délai (par exemple, 5 secondes)
+setTimeout(() => {
+  videoContainer.style.display = 'none';
+  // Ajoutez ici le code pour rediriger l'utilisateur vers la page principale du site, par exemple :
+  // window.location.href = 'page_principale.html';
+}, 5000); // 5000 millisecondes = 5 secondes
